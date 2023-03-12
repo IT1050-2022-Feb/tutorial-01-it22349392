276cffab2d0c2e32849b30696f0a215dbe90ef59
it22349392 <127585678+it22349392@users.noreply.github.com>
2023-03-12 09:26:23 +0000

    main function

---
+++ .ccls-cache/@home@runner@tutorial-01-it22349392-1/Tute01.c
@@ -0,0 +1,15 @@
+/* Exercise 1 - Calculations
+
+   Write a C program to input marks of two subjects. Calculate and print the
+   average of the two marks. */
+
+#include <stdio.h>
+
+int main(void) {
+  int sub1, sub2;
+  printf("Enter marks 1:");
+  scanf("%d", &sub1);
+  printf("Enter marks 2:");
+  scanf("%d", &sub2);
+  return 0;
+}
